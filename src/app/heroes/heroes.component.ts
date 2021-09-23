60a48303ea597391d61321ccecc4faf3bd37ebfa
Djakson Cleber <djakson.filho@gmail.com>
2021-09-23 17:01:35 -0300

    refactors navigation

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,12 +12,10 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
-
-  selectedHero?: Hero;
   
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
   
-  ngOnInit(): void {
+  ngOnInit() {
     this.getHeroes();
   }
 
@@ -25,11 +23,4 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-  
-
 }
